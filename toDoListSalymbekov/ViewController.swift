71e2e6eee5b867a14e6b43143b7d3263cda13454
Morimel <melsovisa77@gmail.com>
2024-01-29 11:08:48 +0600

    Initial Commit

---
+++ toDoListSalymbekov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListSalymbekov
+//
+//  Created by Isa Melsov on 29/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
